58ebea36e9873d5c8c6449259ae2580b45eab688
Ben Meline <benmeline@gmail.com>
2016-05-03 18:35:14 -0500

    Rename starter to awhere

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,10 @@
-// Ionic Starter App
+// Ionic Awhere App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'awhere' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'awhere.controllers' is found in controllers.js
+angular.module('awhere', ['ionic', 'awhere.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
