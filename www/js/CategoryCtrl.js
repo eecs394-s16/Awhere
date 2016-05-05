a6e63094e48a2fbe422ea71ff3b5e3604c44e890
grygoree <GregoryLeung2016@u.northwestern.edu>
2016-05-05 06:07:01 -0500

    Moved each controller into separate file, changed index.html to load each controller.  AppCtrl.js is code that can actually be removed, but is not necessary

---
+++ www/js/CategoryCtrl.js
@@ -0,0 +1,7 @@
+angular.module('awhere.controllers')
+
+.controller('CategoriesCtrl', function($scope) {
+  $scope.categories = [
+    "design", "entrepreneurship", "comp. sci.", "religion", "education", "athletics", "fitness", "business", "music", "life science", "writing"
+  ];
+})
