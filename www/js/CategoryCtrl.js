596948e7655f45826c4a1943b88ff75fda0995d4
ChosunOne <koreanhalo@gmail.com>
2016-05-05 16:05:41 -0500

    Added state params input

--- www/js/CategoryCtrl.js
+++ www/js/CategoryCtrl.js
@@ -1,6 +1,6 @@
 angular.module('awhere.controllers')
 
-.controller('CategoriesCtrl', function($scope) {
+.controller('CategoriesCtrl', function($scope, $stateParams) {
   $scope.categories = [
     "design", "entrepreneurship", "comp. sci.", "religion", "education", "athletics", "fitness", "business", "music", "life science", "writing"
   ];
