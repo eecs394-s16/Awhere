d545be1a617497363648df2f057fe923fe9d89d2
ChosunOne <koreanhalo@gmail.com>
2016-05-25 17:54:46 -0500

    Filtering based on preset

--- www/js/EventsCtrl.js
+++ www/js/EventsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('awhere.controllers')
 
-.controller('EventsCtrl', function($scope, $ionicScrollDelegate,Preset) {  
+.controller('EventsCtrl', function($scope, $ionicScrollDelegate, Preset) {  
 
   $scope.viewStateEnum = {
     HOT: 0,
@@ -14,6 +14,7 @@ angular.module('awhere.controllers')
   $scope.categories = testCategories;
 
   $scope.currentPreset = Preset.find(JSON.parse(localStorage.getItem('currPreset')));
+  console.log($scope.currentPreset);
 
   $scope.toggleView = function(state) {
     $scope.viewState = state;
@@ -28,18 +29,30 @@ angular.module('awhere.controllers')
 
     if ($scope.currentPreset)
     {
-      if (event['price'].indexOf("Free") != -1 || parseInt(event['price']) <= parseInt($scope.currentPreset.price))
-        return true;
-      else
-        return false;
-    }
+      var foundCategory = false;
+      if ($scope.currentPreset.interests.length == 0) {return true};
 
-    if ($scope.category === '') { return true; }
-    if ([event['primary category'], event['secondary category'], event['3rd category']].indexOf($scope.category) > -1) {
-      return true;
-    } else {
-      return false;
+      for (var i = 0; i < $scope.currentPreset.interests.length; i++){
+        $scope.category = $scope.currentPreset.interests[i];
+        if (event['title'] === "Segal Seminar Series: John Bielenberg"){
+          console.log(event['primary category'], $scope.category);
+        }
+
+        if ([event['primary category'], event['secondary category'], event['3rd category']].indexOf($scope.category) > -1) {
+          if (event['price'].indexOf("Free") != -1 || parseInt(event['price']) <= parseInt($scope.currentPreset.price)) {
+            foundCategory = true;
+            console.log("found category");
+            break;
+          }
+        }
+      }
+      return foundCategory;
     }
+    else {
+      console.log("There is no preset");
+      return true;
+    }
+    
   };
 
   $scope.printPrice = function(price) {
@@ -54,7 +67,8 @@ angular.module('awhere.controllers')
 //These will be removed when Database gets implemented.  So i put them at the bottom of the file as globals.
 var testCategories =
 [
-  "design", "entrepreneurship", "comp. sci.", "religion", "education", "athletics", "fitness", "business", "music", "life science", "writing"
+  "Academic:Engineering / Design", "Academic:Business / Economics", "Academic:Sciences", "Other:Unique", "Academic:Education and Organization",
+   "Athletic:Varsity", "Athletic:Club", "Arts:Music", "Academic:English / Journalism / Lit"
 ];
 
 var testEvents =
@@ -66,8 +80,8 @@ var testEvents =
     "location": "Ford Design Studio 1.230",
     "time": "4-5 pm",
     "brief description": "how can thinking wrong be so right?",
-    "primary category": "design",
-    "secondary category": "entrepreneurship",
+    "primary category": "Academic:Engineering / Design",
+    "secondary category": "Academic:Business / Economics",
     "3rd category": ""
   },
   {
@@ -77,9 +91,9 @@ var testEvents =
     "location": "the garage",
     "time": "7-10pm",
     "brief description": "hackathon 4 fun",
-    "primary category": "comp. sci.",
-    "secondary category": "entrepreneurship",
-    "3rd category": "design"
+    "primary category": "Academic:Sciences",
+    "secondary category": "Academic:Business / Economics",
+    "3rd category": "Academic:Engineering / Design"
   },
   {
     "title": "Design Chicago",
@@ -88,7 +102,7 @@ var testEvents =
     "location": "Allen Center",
     "time": "4-6:30 pm",
     "brief description": "3 major design leaders lecturing",
-    "primary category": "design",
+    "primary category": "Academic:Engineering / Design",
     "secondary category": "",
     "3rd category": ""
   },
@@ -99,8 +113,8 @@ var testEvents =
     "location": "Jacobs Center",
     "time": "5:15-8:15 pm",
     "brief description": "The entrepreneurial unicorn.",
-    "primary category": "entrepreneurship",
-    "secondary category": "design",
+    "primary category": "Academic:Business / Economics",
+    "secondary category": "Academic:Engineering / Design",
     "3rd category": ""
   },
   {
@@ -110,8 +124,8 @@ var testEvents =
     "location": "Harris Hall 107",
     "time": "4:00 PM",
     "brief description": "intelligent design?",
-    "primary category": "religion",
-    "secondary category": "education",
+    "primary category": "Other:Unique",
+    "secondary category": "Academic:Education and Organization",
     "3rd category": ""
   },
   {
@@ -121,7 +135,7 @@ var testEvents =
     "location": "South Tennis Courts",
     "time": "11/2/2016",
     "brief description": "women's tennis",
-    "primary category": "athletics",
+    "primary category": "Athletic:Varsity",
     "secondary category": "",
     "3rd category": ""
   },
@@ -132,7 +146,7 @@ var testEvents =
     "location": "SPAC",
     "time": "6:15 - 7:15 am",
     "brief description": "barbell class",
-    "primary category": "fitness",
+    "primary category": "Athletic:Club",
     "secondary category": "",
     "3rd category": ""
   },
@@ -143,8 +157,8 @@ var testEvents =
     "location": "Jacobs Center",
     "time": "3:30 - 5 pm",
     "brief description": "estimations of your organzation",
-    "primary category": "business",
-    "secondary category": "education",
+    "primary category": "Academic:Business / Economics",
+    "secondary category": "Academic:Education and Organization",
     "3rd category": ""
   },
   {
@@ -154,7 +168,7 @@ var testEvents =
     "location": "SPAC",
     "time": "7:30 - 9 pm",
     "brief description": "intermediate belly dancing",
-    "primary category": "fitness",
+    "primary category": "Athletic:Club",
     "secondary category": "",
     "3rd category": ""
   },
@@ -165,7 +179,7 @@ var testEvents =
     "location": "Sheil",
     "time": "5 -  6 pm",
     "brief description": "catholic mass @ sheil",
-    "primary category": "religion",
+    "primary category": "Other:Unique",
     "secondary category": "",
     "3rd category": ""
   },
@@ -176,8 +190,8 @@ var testEvents =
     "location": "Norris",
     "time": "8:00 PM",
     "brief description": "learn the harmonica",
-    "primary category": "music",
-    "secondary category": "education",
+    "primary category": "Arts:Music",
+    "secondary category": "Academic:Education and Organization",
     "3rd category": ""
   },
   {
@@ -187,9 +201,9 @@ var testEvents =
     "location": "2220 campus dr",
     "time": "12 - 1 pm",
     "brief description": "Write about neurobiology",
-    "primary category": "life science",
-    "secondary category": "writing",
-    "3rd category": "education"
+    "primary category": "Academic:Sciences",
+    "secondary category": "Academic:English / Journalism / Lit",
+    "3rd category": "Academic:Education and Organization"
   },
   {
     "title": "Judith Kimble",
@@ -198,8 +212,8 @@ var testEvents =
     "location": "Pancoe",
     "time": "1-2 pm",
     "brief description": "learn about neurobiology",
-    "primary category": "life science",
-    "secondary category": "education",
+    "primary category": "Academic:Sciences",
+    "secondary category": "Academic:Education and Organization",
     "3rd category": ""
   }
   ];
